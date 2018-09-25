981ee40ae7f104ef1e40b6f255ae06b4705cb2e6
ha_hai <hasonhai1996@gmail.com>
2018-09-25 16:10:43 +0700

    commit change branch "foo"

--- app/src/main/java/com/example/ha_hai/customview/custom_view/CustomView.java
+++ app/src/main/java/com/example/ha_hai/customview/custom_view/CustomView.java
@@ -1,5 +1,6 @@
 package com.example.ha_hai.customview.custom_view;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,11 +9,20 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.example.ha_hai.customview.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by ha_hai on 9/16/2018.
@@ -23,6 +33,8 @@ public class CustomView extends View {
     int color = Color.RED;
 
     private Paint mPaint;
+    private int mWidth, mHeight;
+    private CircleImageView circleImageView;
 
     public CustomView(Context context) {
         super(context);
@@ -32,50 +44,23 @@ public class CustomView extends View {
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        setPaint();
 
+        setPaint();
+//        getSizeScreen(context);
+        inflaterCircleImageView(context);
     }
 
     public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
-    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
         int width = MeasureSpec.getSize(widthMeasureSpec);
-        int wMode = MeasureSpec.getMode(widthMeasureSpec);
-
         int height = MeasureSpec.getSize(heightMeasureSpec);
-        int hMode = MeasureSpec.getMode(heightMeasureSpec);
-
-        if (wMode == MeasureSpec.EXACTLY) {
-            Log.d("AAA", "wMode: EXACTLY");
-        } else if (wMode == MeasureSpec.AT_MOST) {
-            Log.d("AAA", "wMode: AT_MOST");
-
-        } else if (wMode == MeasureSpec.UNSPECIFIED) {
-            Log.d("AAA", "wMode: UNSPECIFIED");
-
-        }
-
-        if (hMode == MeasureSpec.EXACTLY) {
-            Log.d("AAA", "hMode: EXACTLY");
-        } else if (hMode == MeasureSpec.AT_MOST) {
-            Log.d("AAA", "hMode: AT_MOST");
-
-        } else if (hMode == MeasureSpec.UNSPECIFIED) {
-            Log.d("AAA", "hMode: UNSPECIFIED");
-
-        }
-
-        Log.d("AAA", "width: " + width + " - height: " + height);
-
 
         setMeasuredDimension(width, height);
     }
@@ -83,7 +68,21 @@ public class CustomView extends View {
     private void setPaint() {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setColor(color);
-        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStyle(Paint.Style.STROKE);
+    }
+
+    private void getSizeScreen(Context context) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        mHeight = displayMetrics.heightPixels;
+        mWidth = displayMetrics.widthPixels;
+    }
+
+    private void inflaterCircleImageView(Context context) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        circleImageView = (CircleImageView) inflater.inflate(R.layout.custom_view, null);
+        circleImageView.measure(MeasureSpec.getSize(circleImageView.getMeasuredWidth()), MeasureSpec.getSize(circleImageView.getMeasuredHeight()));
+        circleImageView.layout(0, 0, 400, 400);
     }
 
 
@@ -91,25 +90,16 @@ public class CustomView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.save();
-        canvas.drawRect(new Rect(0, 0, 200, 200), mPaint);
-
-        canvas.translate(300, 300);
-        canvas.drawCircle(0, 0, 50,  mPaint);
-        canvas.restore();
-
-        canvas.drawRect(0, 50 , 150, 300, mPaint);
+        canvas.translate(mWidth / 2 - 200, mHeight / 2 - 200);
+        circleImageView.draw(canvas);
+        canvas.drawCircle(0, 0, 300, mPaint);
     }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
 
-        Log.d("AAA", "Layout: width: " + getMeasuredWidth() + " - height: " + getMeasuredHeight());
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        mWidth = getWidth();
+        mHeight = getHeight();
     }
 }
